b3c2c903bcb4b86885fc3b7f639b65e0adbfb375
DeynLaengsten <80481149+DeynLaengsten@users.noreply.github.com>
2021-03-14 21:22:28 +0100

    Play system sound when timer is finished

--- Two Minute Tea Timer/Models/TimerModel.swift
+++ Two Minute Tea Timer/Models/TimerModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class TimerModel: ObservableObject {
     var timer = Timer()
@@ -16,18 +17,32 @@ class TimerModel: ObservableObject {
     @Published var seconds = 0
     
     func startTimer() {
+        timer.invalidate()
         minutes = 1
         tenSeconds = 5
         seconds = 9
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-    
+
+    func startTimer(min: Int, tenSec: Int, sec: Int) {
+        timer.invalidate()
+        minutes = min
+        tenSeconds = tenSec
+        seconds = sec
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+ 
     @objc func updateTimer() {
         if(minutes == 0 && tenSeconds == 0 && seconds == 0) {
             timer.invalidate()
             minutes = 2
             tenSeconds = 0
             seconds = 0
+            
+            let systemSoundID: SystemSoundID = 1005
+            
+            AudioServicesPlaySystemSound(systemSoundID)
+            
         } else {
             seconds -= 1
             if(seconds < 0) {
