b52eafca607585385fe60f00fccfdbbd92fe8ce3
DeynLaengsten <80481149+DeynLaengsten@users.noreply.github.com>
2021-03-14 21:22:59 +0100

    circle shape around timer

--- Two Minute Tea Timer/Views/ContentView.swift
+++ Two Minute Tea Timer/Views/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     
@@ -13,12 +14,30 @@ struct ContentView: View {
     
     var body: some View {
         
-        Text(timerModel.getTime())
-            .padding()
-    
+        ZStack {
+            Circle()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200, alignment: .center)
+                .foregroundColor(Color(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)))
+            Circle()
+                .trim(from: 0.0, to: 1.0)
+                .fill(Color.blue, style: FillStyle())
+                .frame(width: 200, height: 200, alignment: .center)
+                
+            Button(action: { timerModel.startTimer()
+            })
+            {
+                Text(timerModel.getTime())
+                    .fontWeight(.heavy)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .padding()
+            }
+            
             .onAppear(perform: {
                 timerModel.startTimer()
-            })
+        })
+        }
     }
 }
 
