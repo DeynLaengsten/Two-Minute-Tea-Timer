8203e4454b23e0259827b1118cce546bad80eecc
DeynLaengsten <80481149+DeynLaengsten@users.noreply.github.com>
2021-03-18 19:22:24 +0100

    add color struct

---
+++ Two Minute Tea Timer/Models/MyColors.swift
@@ -0,0 +1,16 @@
+//
+//  MyColors.swift
+//  Two Minute Tea Timer
+//
+//  Created by Nico Bertsch on 17.03.21.
+//
+
+import SwiftUI
+
+struct MyColors {
+    let backgroundColor: Color = Color(#colorLiteral(red: 0.9921568627, green: 0.9450980392, blue: 0.8392156863, alpha: 1))
+    let progressBarColor: Color = Color(#colorLiteral(red: 0.8549019608, green: 0.4470588235, blue: 0.2352941176, alpha: 1))
+    let timerButtonColor: Color = Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    let circleColor: Color = Color(#colorLiteral(red: 0.7647058824, green: 0.6196078431, blue: 0.3607843137, alpha: 1))
+    let pulsColor: Color = Color(#colorLiteral(red: 0.568627451, green: 0.03529411765, blue: 0.1176470588, alpha: 1))
+}
