c2507b771b72124e278c845f1141b39c647c1175
DeynLaengsten <80481149+DeynLaengsten@users.noreply.github.com>
2021-03-18 19:23:51 +0100

    improved time calculation and return values for animations

--- Two Minute Tea Timer/Models/TimerModel.swift
+++ Two Minute Tea Timer/Models/TimerModel.swift
@@ -11,53 +11,52 @@ import AVFoundation
 
 class TimerModel: ObservableObject {
     var timer = Timer()
+    @Published var duration: Int
+    let initTime: Int
     
-    @Published var minutes = 2
-    @Published var tenSeconds = 0
-    @Published var seconds = 0
+    init(startTime: Int) {
+        duration = startTime
+        initTime = startTime
+    }
     
     func startTimer() {
         timer.invalidate()
-        minutes = 1
-        tenSeconds = 5
-        seconds = 9
+        duration = initTime - 1
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
 
-    func startTimer(min: Int, tenSec: Int, sec: Int) {
+    func startTimer(with seconds: Int) {
         timer.invalidate()
-        minutes = min
-        tenSeconds = tenSec
-        seconds = sec
+        duration = seconds - 1
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
  
     @objc func updateTimer() {
-        if(minutes == 0 && tenSeconds == 0 && seconds == 0) {
+        if(duration == 0) {
             timer.invalidate()
-            minutes = 2
-            tenSeconds = 0
-            seconds = 0
-            
+            duration = initTime
+
             let systemSoundID: SystemSoundID = 1005
             
             AudioServicesPlaySystemSound(systemSoundID)
             
         } else {
-            seconds -= 1
-            if(seconds < 0) {
-                seconds = 9
-                tenSeconds -= 1
-                if(tenSeconds < 0) {
-                    tenSeconds = 5
-                    minutes -= 1
-                }
-            }
+            duration -= 1
         }
         
     }
     
-     func getTime() -> String {
-        return "\(minutes):\(tenSeconds)\(seconds)"
+    func getTime() -> String {
+        return String(format: "%02d:%02d", duration / 60, duration % 60)
+    }
+    
+    func getPercentage() -> CGFloat {
+        return (CGFloat(duration) / CGFloat(initTime)) * CGFloat(100.0)
+    }
+    
+    func getTimerIsRunning() -> Bool {
+        print(duration < initTime)
+        return (duration < initTime ? true : false)
+        
     }
 }
